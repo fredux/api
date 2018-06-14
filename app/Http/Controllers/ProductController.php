cab00caeedcbb1cab03072543b6fde3f13340015
fredux <vitor.fred@gmail.com>
2018-06-14 16:16:23 -0400

    Cr4iacao da Resource Review

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Model\Product;
+use App\Model\Review;
 use Illuminate\Http\Request;
+use App\Http\Resources\Product\ProductCollection;
+use App\Http\Resources\Product\ProductResource;
 
 class ProductController extends Controller
 {
@@ -14,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return ProductCollection::collection(Product::all());
     }
 
     /**
@@ -46,7 +49,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return new ProductResource($product);
     }
 
     /**
